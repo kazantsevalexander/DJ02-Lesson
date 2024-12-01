a5960468c8c52687511fc4a1630407b4a3fb7d2a
kazantsevalexander <kazantsevalexander@gmail.com>
2024-12-01 18:37:48 +0300

    add news

--- birdcat_project/main/views.py
+++ birdcat_project/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'main/home.html')
+    return render(request, 'main/index.html')
 
 def secondary(request):
     return render(request, 'main/secondary.html')
